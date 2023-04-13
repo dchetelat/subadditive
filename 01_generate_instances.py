724e19f4fed8986f6055f3044dedfc91ea9c6cd6
Didier Chetelat <didier.chetelat@polymtl.ca>
2023-04-13 11:49:13 -0400

    Added seed in instance generation

--- 01_generate_instances.py
+++ 01_generate_instances.py
@@ -8,18 +8,22 @@ def create_instances(instances, folder):
         instance = next(instances)
         instance.write_problem(str(folder/f"instance_{instance_count}.lp"))
 
-instances = ecole.instance.SetCoverGenerator()
+
+rng = ecole.spawn_random_generator()
+rng.seed(0)
+
+instances = ecole.instance.SetCoverGenerator(rng=rng)
 folder = Path("data/instances/setcover")
 create_instances(instances, folder)
 
-instances = ecole.instance.CombinatorialAuctionGenerator()
+instances = ecole.instance.CombinatorialAuctionGenerator(rng=rng)
 folder = Path("data/instances/cauctions")
 create_instances(instances, folder)
 
-instances = ecole.instance.IndependentSetGenerator(n_nodes=300)
+instances = ecole.instance.IndependentSetGenerator(n_nodes=300, rng=rng)
 folder = Path("data/instances/indset")
 create_instances(instances, folder)
 
-instances = ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=20, n_facilities=10, continuous_assignment=False)
+instances = ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=20, n_facilities=10, continuous_assignment=False, rng=rng)
 folder = Path("data/instances/facilities")
 create_instances(instances, folder)
